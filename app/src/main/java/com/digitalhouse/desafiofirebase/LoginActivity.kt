948bfb1fb98304b15f1dd74a901be1f9c03525a0
AlineCruzDEV <linegcruz@gmail.com>
2021-01-24 15:50:25 -0300

    Implementacao da tela de Login

--- app/src/main/java/com/digitalhouse/desafiofirebase/LoginActivity.kt
+++ app/src/main/java/com/digitalhouse/desafiofirebase/LoginActivity.kt
@@ -1,11 +1,37 @@
 package com.digitalhouse.desafiofirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.digitalhouse.desafiofirebase.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cvLogin.btnLogin.setOnClickListener {
+            callHome()
+        }
+
+        binding.cvLogin.tvCreateAccout.setOnClickListener {
+            callRegister()
+        }
+
+    }
+
+    private fun callRegister() {
+        var intent = Intent(application, CadastroActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun callHome() {
+        finishAffinity()
+        var intent = Intent(application, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
