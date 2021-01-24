f48f29d8c0755114290728b83a3932713d7da077
AlineCruzDEV <linegcruz@gmail.com>
2021-01-24 16:03:18 -0300

    Implementacao da tela de Cadastro

--- app/src/main/java/com/digitalhouse/desafiofirebase/CadastroActivity.kt
+++ app/src/main/java/com/digitalhouse/desafiofirebase/CadastroActivity.kt
@@ -1,11 +1,26 @@
 package com.digitalhouse.desafiofirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.digitalhouse.desafiofirebase.databinding.ActivityCadastroBinding
 
 class CadastroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCadastroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        binding = ActivityCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cvCadastro.btnCreateAccount.setOnClickListener {
+            callHome()
+        }
+    }
+    private fun callHome() {
+        finishAffinity()
+        var intent = Intent(application, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
